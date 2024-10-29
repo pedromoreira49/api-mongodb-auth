4b5943dc185bf152c7886fba0d9545697e58b5f0
Pedro Moreira <jpedrostonemoreira@gmail.com>
2024-10-29 12:10:56 -0300

    feat: adicionado método para verificar permissões do usuário no middleware

--- src/middlewares/GlobalMiddleware.js
+++ src/middlewares/GlobalMiddleware.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
+import { permissions } from '../security/Security.js'
 
 dotenv.config()
 
@@ -28,5 +29,35 @@ export const isAdmin = (req, res, next) => {
     res.status(500).send({
       message: err.message
     })
+  }
+}
+
+export const hasPermission = (resource, action) => {
+  return (req, res, next) => {
+    try {
+      const {authorization} = req.headers;
+      const parts = authorization.split(" ");
+  
+      jwt.verify(parts[1], process.env.SECRET_JWT, async (err, decoded) => {
+        if (err) {
+          return res.status(401).send({
+            message: "Invalid Token!"
+          })
+        }
+  
+        const role = decoded.role;
+        const rolePermissions = permissions[role];
+
+        if (!rolePermissions || !rolePermissions[resource]?.includes(action)) {
+            return res.status(403).json({ message: 'Access denied' });
+        }
+
+        next();
+      })
+    } catch (err) {
+      res.status(500).send({
+        message: err.message
+      })
+    }
   }
 }
